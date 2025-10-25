199b5e753ab17a5e660e969e5cc0916c833d2efe
kabuspl <me@kabus.ovh>
2025-10-25 15:13:17 +0200

    translate to polish

--- ui/lang/pl_PL.ts
+++ ui/lang/pl_PL.ts
@@ -1,149 +1,150 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="pl" sourcelanguage="en">
-<context>
+  <context>
     <name>AudioPage</name>
     <message>
-        <location filename="../AudioPage.qml" line="9"/>
-        <source>Audio Tracks</source>
-        <translation type="unfinished"></translation>
+      <location filename="../AudioPage.qml" line="9"/>
+      <source>Audio Tracks</source>
+      <translation>Ścieżki Dźwięku</translation>
     </message>
     <message>
-        <location filename="../AudioPage.qml" line="14"/>
-        <source>Add track</source>
-        <translation type="unfinished"></translation>
+      <location filename="../AudioPage.qml" line="14"/>
+      <source>Add track</source>
+      <translation>Dodaj ścieżkę</translation>
     </message>
     <message>
-        <location filename="../AudioPage.qml" line="25"/>
-        <source>Simple</source>
-        <translation type="unfinished"></translation>
+      <location filename="../AudioPage.qml" line="25"/>
+      <source>Simple</source>
+      <translation>Proste</translation>
     </message>
     <message>
-        <location filename="../AudioPage.qml" line="34"/>
-        <source>Advanced</source>
-        <translation type="unfinished"></translation>
+      <location filename="../AudioPage.qml" line="34"/>
+      <source>Advanced</source>
+      <translation>Zaawansowane</translation>
     </message>
     <message>
-        <location filename="../AudioPage.qml" line="97"/>
-        <source>Record system sound</source>
-        <translation type="unfinished"></translation>
+      <location filename="../AudioPage.qml" line="97"/>
+      <source>Record system sound</source>
+      <translation>Nagrywaj dźwięk systemu</translation>
     </message>
     <message>
-        <location filename="../AudioPage.qml" line="105"/>
-        <source>Record microphone</source>
-        <translation type="unfinished"></translation>
+      <location filename="../AudioPage.qml" line="105"/>
+      <source>Record microphone</source>
+      <translation>Nagrywaj mikrofon</translation>
     </message>
     <message>
-        <location filename="../AudioPage.qml" line="113"/>
-        <source>Merge system &amp; microphone tracks</source>
-        <translation type="unfinished"></translation>
+      <location filename="../AudioPage.qml" line="113"/>
+      <source>Merge system &amp; microphone tracks</source>
+      <translation>Złącz ścieżki systemu i mikrofonu</translation>
     </message>
     <message>
-        <location filename="../AudioPage.qml" line="143"/>
-        <source>Simple audio settings are not available when custom audio tracks are configured.</source>
-        <translation type="unfinished"></translation>
+      <location filename="../AudioPage.qml" line="143"/>
+      <source>Simple audio settings are not available when custom audio tracks are configured.</source>
+      <translation>Proste ustawienia dźwięku nie są dostępne, gdy skonfigurowane są własne ścieżki.</translation>
     </message>
     <message>
-        <location filename="../AudioPage.qml" line="153"/>
-        <source>Reset audio tracks</source>
-        <translation type="unfinished"></translation>
+      <location filename="../AudioPage.qml" line="153"/>
+      <source>Reset audio tracks</source>
+      <translation>Zresetuj ścieżki dźwięku</translation>
     </message>
     <message>
-        <location filename="../AudioPage.qml" line="221"/>
-        <source>Default Microphone</source>
-        <translation type="unfinished"></translation>
+      <location filename="../AudioPage.qml" line="221"/>
+      <source>Default Microphone</source>
+      <translation>Domyślny mikrofon</translation>
     </message>
     <message>
-        <location filename="../AudioPage.qml" line="224"/>
-        <source>System Sound</source>
-        <translation type="unfinished"></translation>
+      <location filename="../AudioPage.qml" line="224"/>
+      <source>System Sound</source>
+      <translation>Dźwięk systemu</translation>
     </message>
     <message>
-        <location filename="../AudioPage.qml" line="277"/>
-        <source>Add source</source>
-        <translation type="unfinished"></translation>
+      <location filename="../AudioPage.qml" line="277"/>
+      <source>Add source</source>
+      <translation>Dodaj źródło</translation>
     </message>
     <message>
-        <location filename="../AudioPage.qml" line="293"/>
-        <source>Add audio source</source>
-        <translation type="unfinished"></translation>
+      <location filename="../AudioPage.qml" line="293"/>
+      <source>Add audio source</source>
+      <translation>Dodaj źródło dźwięku</translation>
     </message>
     <message>
-        <location filename="../AudioPage.qml" line="318"/>
-        <source>Default microhpone</source>
-        <translation type="unfinished"></translation>
+      <location filename="../AudioPage.qml" line="318"/>
+      <source>Default microhpone</source>
+      <translation>Domyślny mikrofon</translation>
     </message>
     <message>
-        <location filename="../AudioPage.qml" line="324"/>
-        <source>System sound</source>
-        <translation type="unfinished"></translation>
+      <location filename="../AudioPage.qml" line="324"/>
+      <source>System sound</source>
+      <translation>Dźwięk systemu</translation>
     </message>
     <message>
-        <location filename="../AudioPage.qml" line="329"/>
-        <source>Application:</source>
-        <translation type="unfinished"></translation>
+      <location filename="../AudioPage.qml" line="329"/>
+      <source>Application:</source>
+      <translation>Aplikacja:</translation>
     </message>
     <message>
-        <location filename="../AudioPage.qml" line="342"/>
-        <source>Other device:</source>
-        <translation type="unfinished"></translation>
+      <location filename="../AudioPage.qml" line="342"/>
+      <source>Other device:</source>
+      <translation>Inne urządzenie:</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>MainPage</name>
     <message>
-        <location filename="../MainPage.qml" line="12"/>
-        <source>Settings</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="12"/>
+      <source>Settings</source>
+      <translation>Ustawienia</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="16"/>
-        <source>Record replays</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="16"/>
+      <source>Record replays</source>
+      <translation>Nagrywaj powtórki</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="36"/>
-        <source>Video source:</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="36"/>
+      <source>Video source:</source>
+      <translation>Źródło obrazu:</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="44"/>
-        <source>Default (%1)</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="44"/>
+      <source>Default (%1)</source>
+      <translation>Domyślne (%1)</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="72"/>
-        <source>Directory:</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="72"/>
+      <source>Directory:</source>
+      <translation>Katalog:</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="93"/>
-        <source>Choose replay directory</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="93"/>
+      <source>Choose replay directory</source>
+      <translation>Wybierz katalog powtórek</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="100"/>
-        <source>Save videos:</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="100"/>
+      <source>Save videos:</source>
+      <translation>Zapisuj nagrania:</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="108"/>
-        <source>In directories named after the current full-screen app</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="108"/>
+      <source>In directories named after the current full-screen app</source>
+      <translation>W katalogach nazwanych jak obecna
+pełnoekranowa aplikacja</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="114"/>
-        <source>Directly in the directory selected above</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="114"/>
+      <source>Directly in the directory selected above</source>
+      <translation>Bezpośrednio w katalogu wybranym powyżej</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="120"/>
-        <source>Using custom pattern</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="120"/>
+      <source>Using custom pattern</source>
+      <translation>Używając niestandardowego wzoru</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="136"/>
-        <source>Available variables:
+      <location filename="../MainPage.qml" line="136"/>
+      <source>Available variables:
 %app% - title of the current full-screen window or unknown
 %year% - current year
 %month% - current month
@@ -151,55 +152,62 @@
 %hour% - current hour
 %minute% - current minute
 %second% - current second</source>
-        <translation type="unfinished"></translation>
+      <translation>Dostępne zmienne:
+%app% - nazwa obecnej pełnoekranowej aplikacji lub "unknown"
+%year% - obecny rok
+%month% - obecny miesiąc
+%day% - obecny dzień
+%hour% - obecna godzina
+%minute% - obecna minuta
+%second% - obecna sekunda</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="149"/>
-        <source>Duration:</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="149"/>
+      <source>Duration:</source>
+      <translation>Długość:</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="165"/>
-        <source>secs</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="165"/>
+      <source>secs</source>
+      <translation>sekund</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="170"/>
-        <source>Container:</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="170"/>
+      <source>Container:</source>
+      <translation>Format:</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="181"/>
-        <source>Codec:</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="181"/>
+      <source>Codec:</source>
+      <translation>Kodek:</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="192"/>
-        <source>Quality:</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="192"/>
+      <source>Quality:</source>
+      <translation>Jakość:</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="203"/>
-        <source>Framerate:</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="203"/>
+      <source>Framerate:</source>
+      <translation>Klatki na sekundę:</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="228"/>
-        <source>Clear buffer when saving</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="228"/>
+      <source>Clear buffer when saving</source>
+      <translation>Czyść bufor przy zapisie</translation>
     </message>
     <message>
-        <location filename="../MainPage.qml" line="237"/>
-        <source>Edit audio tracks</source>
-        <translation type="unfinished"></translation>
+      <location filename="../MainPage.qml" line="237"/>
+      <source>Edit audio tracks</source>
+      <translation>Edytuj ścieżki dźwięku</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>settings</name>
     <message>
-        <location filename="../settings.qml" line="6"/>
-        <source>TrayPlay Settings</source>
-        <translation type="unfinished"></translation>
+      <location filename="../settings.qml" line="6"/>
+      <source>TrayPlay Settings</source>
+      <translation>Ustawienia TrayPlay</translation>
     </message>
-</context>
+  </context>
 </TS>
